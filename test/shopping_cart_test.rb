3d19cc766dbde496c33655f164206b1eacf611fa
Chris Allbritton <cla.inbox@gmail.com>
2020-07-08 12:47:45 -0400

    Create shopping_cart_test

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,67 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/product'
+require '../lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_instance_of ShoppingCart, cart
+  end
+
+  def test_it_has_a_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.name
+  end
+
+  def test_it_has_a_capacity
+    # skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "30items", cart.capacity
+  end
+
+  def test_products_are_empty
+    # skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal [], cart.products
+  end
+
+  def test_it_can_add_products
+    #skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal [product1, product2], cart.products
+
+  end
+
+  def test_it_has_details
+    skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "{:name =>'King Soopers', :capacity=>30}", cart.details
+  end
+
+  def test_total_number_of_products
+    #skip
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.total_number_of_products
+
+
+  end
+
+
+end
